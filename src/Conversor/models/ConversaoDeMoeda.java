0bdc48cf86f00b9d5792b1e49913944cb7907902
Deivid Souza <deividsouza252@gmail.com>
2024-07-03 16:25:07 -0300

    V1.0

---
+++ src/Conversor/models/ConversaoDeMoeda.java
@@ -0,0 +1,57 @@
+package Conversor.models;
+
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConversaoDeMoeda {
+    String apiKey = "66a39028a323ebdaccb55d3d";
+    @SerializedName("base_code")
+    String countryCoin;
+    @SerializedName("target_code")
+    String convertedCountryCoin;
+    @SerializedName("conversion_rate")
+    double taxCoin;
+    @SerializedName("conversion_result")
+    double convertedValue;
+
+    public String getCountryCoin() {
+        return countryCoin;
+    }
+
+    public String getConvertedCountryCoin() {
+        return convertedCountryCoin;
+    }
+
+    public double getTaxCoin() {
+        return taxCoin;
+    }
+
+    public double getConvertedValue() {
+        return convertedValue;
+    }
+
+    public void conversao(String originCoin, String convertedCoin,  double value) throws IOException, InterruptedException {
+        String endereco = "https://v6.exchangerate-api.com/v6/"+apiKey+"/pair/"+ originCoin +"/"+ convertedCoin +"/"+ value;
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+        String json = response.body();
+        try {
+            Gson gson = new Gson();
+            ConversaoDeMoeda moeda = gson.fromJson(json, ConversaoDeMoeda.class);
+            System.out.println("O valor " + value + " " + originCoin + ", convertido em " + convertedCoin + " é de: ");
+            System.out.println(moeda.getConvertedValue());
+        }catch (Exception e){
+            System.out.println("Aconteceu algun erro.");
+        }
+    }
+}
---
+++ src/Principal.java
@@ -0,0 +1,67 @@
+import Conversor.models.ConversaoDeMoeda;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        int option = 0;
+        ConversaoDeMoeda coin = new ConversaoDeMoeda();
+
+        String menssage = """
+                              --------------------------------------------------------------
+                              Seja bem-vindo ao conversor de moedas.
+                              
+                              escolha uma opção:
+                              
+                              1 - Converter do Real Brasileiro para o Dolar.
+                              2 - Converter do Dolar para o Real Brasileiro.
+                              3 - Converter do Peso Argentino para o Dolar.
+                              4 - Converter do Dolar para o peso Argentino.
+                              5 - Converter do Euro para o Dolar.
+                              6 - Converter do Dolar para o Euro.
+                              7 - Encerrar.
+                              """;
+        System.out.println(menssage);
+        while(option!=7){
+            Scanner optionEntry = new Scanner(System.in);
+            option = optionEntry.nextInt();
+            switch (option){
+                case 1:
+                    System.out.println("Insira o valor para conversão:");
+                    coin.conversao("BRL","USD", optionEntry.nextDouble());
+                    System.out.println(menssage);
+                    break;
+                case 2:
+                    System.out.println("Insira o valor para conversão:");
+                    coin.conversao("USD","BRL", optionEntry.nextDouble());
+                    System.out.println(menssage);
+                    break;
+                case 3:
+                    System.out.println("Insira o valor para conversão:");
+                    coin.conversao("ARS","USD", optionEntry.nextDouble());
+                    System.out.println(menssage);
+                    break;
+                case 4:
+                    System.out.println("Insira o valor para conversão:");
+                    coin.conversao("USD","ARS", optionEntry.nextDouble());
+                    System.out.println(menssage);
+                    break;
+                case 5:
+                    System.out.println("Insira o valor para conversão:");
+                    coin.conversao("EUR","USD", optionEntry.nextDouble());
+                    System.out.println(menssage);
+                    break;
+                case 6:
+                    System.out.println("Insira o valor para conversão:");
+                    coin.conversao("USD","EUR", optionEntry.nextDouble());
+                    System.out.println(menssage);
+                    break;
+            }if ((option < 1) || (option > 7)){
+                System.out.println("Opção invalida.");
+                System.out.println(menssage);
+            }
+        }
+        System.out.println("O sistema foi finalizado.");
+    }
+}
